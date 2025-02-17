367401cd43ce7e28e29132ce3b712c3037aa14cd
antonioisaacdias <antonioisaacvd@gmail.com>
2025-02-17 11:12:06 -0300

    ajustes no bootstrap

--- views/static/js/index.js
+++ views/static/js/index.js
@@ -1,4 +1,4 @@
 if (window.location.pathname == '/'){
     const btnAddNewExpense = document.getElementById('btn-add-new-expense')
-    btnAddNewExpense.hidden = false
-}
+    btnAddNewExpense.hidden = false }
+
